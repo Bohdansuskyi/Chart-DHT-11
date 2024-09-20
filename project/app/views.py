661409fd81a3fef1a69219a6ba7112b63d61991d
Bohdansuskyi <bohdansyskiy1214@gmail.com>
2024-09-20 20:44:28 +0200

    chart use data from database

--- project/app/views.py
+++ project/app/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from .models import Information
 
 def index(request):
     return render(request, "app/index.html")
 
 def chart(request):
-    return render(request, "app/chart.html")
+    data = Information.objects.all()
+
+    # get data from model
+    data_time = [str(entry.time) for entry in data]
+    temperature_data = [entry.temperature for entry in data]
+    humidity_data = [entry.humidity for entry in data]
+
+    return render(request, "app/chart.html", {
+        'data_time': data_time,
+        'temperature_data': temperature_data,
+        'humidity_data':humidity_data
+    })
